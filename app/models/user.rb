4704f79987df945d5751eda48f22459b47f70bbd
Tracy Reuther <tracyreuther@gmail.com>
2019-09-15 10:35:50 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 #has_secure_password is a macro that adds additional methods to your app.
-#the #authenticate method is added and will check for correct password
+#the #authenticate method is added and validates password match
 
 class User < ActiveRecord::Base
 	has_secure_password
