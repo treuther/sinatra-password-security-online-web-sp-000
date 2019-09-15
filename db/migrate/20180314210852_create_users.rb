692cf838a9f9bcbe5db56f9639ed9fd72ea75a1d
Tracy Reuther <tracyreuther@gmail.com>
2019-09-15 09:54:07 +0000

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,15 @@
+#This creates the users table
+#Before filling this in, you must create the database in the terminal:
+#rake db:migrate NAME=table_name
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
   end
-  
+
   def down
+    drop_table :users
   end
 end
